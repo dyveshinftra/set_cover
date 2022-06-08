d8fc8d60e944811388516e9672dfa46617c702fc
dyveshinftra <56993580+dyveshinftra@users.noreply.github.com>
2022-06-08 16:48:50 +0200

    example universe

--- src/set_cover.erl
+++ src/set_cover.erl
@@ -15,11 +15,41 @@
 -include_lib("eunit/include/eunit.hrl").
 
 % brute-force search algorithm
-brute_force(_S) -> [].
+brute_force(M) ->
+    % S stays a map to keep track of the keys
+    S = maps:map(fun(_Key, Value) -> sets:from_list(Value) end, M),
+    U = sets:union(maps:values(S)),
+    C = sets:new(),
+
+    % start brute-force search algorithm
+    brute_force(U, S, C, [], maps:keys(S)).
+
+% found a cover
+brute_force(U, _, U, CKeys, _) -> CKeys;
+
+% no cover found
+brute_force(_, S, _, _, []) -> maps:keys(S);
+
+% try with and without next subset
+brute_force(U, S, C, CKeys, [H|T]) ->
+    % with subset from S
+    CKeys1 = brute_force(U, S, sets:union(C, maps:get(H, S)), [H|CKeys], T),
+
+    % without subset from S
+    CKeys2 = brute_force(U, S, C, CKeys, T),
+
+    % prefer smallest cover
+    case length(CKeys1) < length(CKeys2) of
+        true -> CKeys1;
+        false -> CKeys2
+    end.
 
 brute_force_test() ->
     % empty universe
     [] = brute_force(#{}),
+
+    % example universe
+    [one,four] = brute_force(#{one=>[1,2,3], two=>[2,4], three=>[3,5], four=>[4,5]}),
 
     % all done
     ok.
