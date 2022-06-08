78abbbf8fdb65332023c1a309c516a2c32d17b56
dyveshinftra <56993580+dyveshinftra@users.noreply.github.com>
2022-06-08 12:59:56 +0200

    empty universe

--- src/set_cover.erl
+++ src/set_cover.erl
@@ -1,3 +1,25 @@
--module(set_cover).
+% Set Cover Problem
+%
+% Example:
+%   U = {1, 2, 3, 4, 5}
+%   S = {{1, 2, 3}, {2, 4}, {3, 5}, {4, 5}}
+%   C = {{1, 2, 3}, {4, 5}}
+%
+% Given a set of elements (the universe U) and a family of subsets of U (S), a
+% cover (C) is a subset of S whose union matches the universe.
+%
+% Finding the cover with the fewest sets, the optimization problem, is NP-hard.
 
--export([]).
+-module(set_cover).
+-export([brute_force/1]).
+-include_lib("eunit/include/eunit.hrl").
+
+% brute-force search algorithm
+brute_force(_S) -> [].
+
+brute_force_test() ->
+    % empty universe
+    [] = brute_force(#{}),
+
+    % all done
+    ok.
